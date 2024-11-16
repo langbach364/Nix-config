879a5ba3596bf8eeba546c0a294d4d8f32b1e01c
installer <installer@gmail.com>
2024-11-16 16:21:49 +0700

    use new version bamboo-core

--- application/Fcitx5-bamboo.nix
+++ application/Fcitx5-bamboo.nix
@@ -1,12 +1,59 @@
 { config, pkgs, ... }:
 
-{
+let
+  fcitx5-bamboo = pkgs.fetchFromGitHub {
+    owner = "fcitx";
+    repo = "fcitx5-bamboo";
+    rev = "1.0.6";
+    sha256 = "1k7c14kpskcs68l0vdydalsfsd122wymhd47l65m22p1rgch0mq2";
+  };
+
+  bamboo-core = pkgs.fetchFromGitHub {
+    owner = "BambooEngine";
+    repo = "bamboo-core";
+    rev = "b2e49a2b48c7d3772a3673142a7747eccd9d5f79";
+    sha256 = "026z1kppw9zvb0p4s9kfjnbj4x3p3fjrf288q3cbiaikimk594q1";
+  };
+in {
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5
-      fcitx5-bamboo
+      (pkgs.stdenv.mkDerivation {
+        pname = "fcitx5-bamboo";
+        version = "1.0.6";
+        
+        src = fcitx5-bamboo;
+        
+        nativeBuildInputs = with pkgs; [
+          cmake
+          extra-cmake-modules
+          pkg-config
+          go
+          git
+        ];
+        
+        buildInputs = with pkgs; [
+          fcitx5
+          gettext
+          fmt
+        ];
+
+        GOCACHE = "/tmp/go-cache";
+        GOPATH = "/tmp/go";
+        
+        preConfigurePhases = ["preBuildPhase"];
+        preBuildPhase = ''
+          mkdir -p bamboo
+          cp -r ${bamboo-core}/* bamboo/bamboo-core/
+        '';
+
+        cmakeFlags = [
+          "-DCMAKE_INSTALL_PREFIX=${placeholder "out"}"
+          "-DCMAKE_INSTALL_LIBDIR=lib"
+        ];
+      })
       fcitx5-configtool
     ];
   };
